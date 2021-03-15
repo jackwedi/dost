ed1566220ad9fecf669a84aad8f07e1f4b983a7d
Jack Karuneswaran <jack.karuneswaran@gmail.com>
2021-03-15 23:49:08 +0100

    feat: create group and join group routes

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -6,14 +6,37 @@ router.route('/').get((req, res) => {
     res.json(["Jack", "John", "Paul"])
 });
 
-router.route('/create').post((req, res) => {
-    // Check if group exists ?
-    // const name = "1stGroup";
-    const users = [new  User({pseudo: "Jack", dateOfBirth: new Date(1994, 9, 13)}), new  User({pseudo: "John"}), new  User({pseudo: "Jeff"})]
-    const newGroup = new Group({pseudo: "1stGroup", users });
-    newGroup.save()
-    .then(() => res.json("Group Created"))
+router.route('/create').post(async (req, res) => {
+    const param = req.body;
+    Group.create({pseudo: param.pseudo, users: [new User({ googleId: param.user.googleId, name: param.user.name, dateOfBirth: param.user.dateOfBirth})]})
+    .then(() => res.json(`Created ${param.pseudo} with ${param.users.join(`,`)}`))
     .catch(err => res.status(400).json('Error: ' + err));
 });
+
+router.route('/join').post(async (req, res) => {
+    const param = req.body;
+
+    Group.updateOne({
+        _id: param.groupID
+    }, {
+        $push: {
+            users: new User({ googleId: param.user.googleId, name: param.user.name, dateOfBirth: param.user.dateOfBirth}) }
+        },
+        {
+            new: true,
+            upsert: true
+        }, 
+        (err, response) => {
+            if (err) res.status(400).json('Error: ' + err)
+            else {
+                res.json("OK");
+            }
+        } 
+    );
+
+
+});
+
+
 
 module.exports = router;
