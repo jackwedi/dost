332482dc556959ba62bff30b705dc813e932abc4
Jack Karuneswaran <jack.karuneswaran@gmail.com>
2021-04-08 09:43:53 +0200

    fix: control birthday input

--- front/src/components/birthdayModal.js
+++ front/src/components/birthdayModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Modal, Header, Icon, Form, Input, Select, Button } from "semantic-ui-react";
+import { Modal, Header, Message, Form, Input, Select, Button } from "semantic-ui-react";
 
 const months = [
     { key: 'jan', text: 'January', value: 'January' },
@@ -20,34 +20,35 @@ const months = [
 class BirthdayModal extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { month: null, day: null, year: null, validDay: null, validYear: null, validForm: null };
+        this.state = { month: null, day: null, year: null, validDay: null, validYear: null, validForm: null, futureDOB: null };
     }
 
     get parsedDay() {
-        return parseInt(this.state.day);
+        return parseInt(this.state.day, 10);
     }
 
     get parsedYear() {
-        return  parseInt(this.state.year);
+        return  parseInt(this.state.year, 10);
     }
 
     checkInput() {
-        const incorrectDay = isNaN(this.parsedDay) || this.state.day.length > 2 || this.parsedDay > 31 || !this.parsedDay;
-        const incorrectYear = isNaN(this.parsedYear) || this.state.year.length !== 4 || !this.parsedYear;
+        const incorrectDay = isNaN(this.parsedDay) || !isFinite(this.state.day) || this.state.day.length > 2 || this.parsedDay > 31 || !this.parsedDay;
+        const incorrectYear = isNaN(this.parsedYear) || !isFinite(this.state.year) || this.state.year.length !== 4;
+
         let validForm = !incorrectYear && !incorrectDay && this.state.month;
         
+        let futureDOB = null;
         if (validForm) {
             try {
                 const computedDate = new Date(`${this.state.month} ${this.parsedDay}, ${this.parsedYear}`);
-                const now = Date.now();
+                const now = new Date(Date.now());
                 validForm = validForm && (computedDate < now);
-                console.log(validForm)
+                futureDOB = !(computedDate < now);
             } catch (e) {
                 console.log("CANT PRODUCE DATE")
             }
         }
-        this.setState({validDay: !incorrectDay, validYear:!incorrectYear, validForm });
-
+        this.setState({validDay: !incorrectDay, validYear: !incorrectYear, validForm , futureDOB });
     }
 
     onMonthChange(ev, data) {
@@ -83,15 +84,13 @@ class BirthdayModal extends React.Component {
                 size='small'
                 closeOnDimmerClick={false}
                 closeOnEscape={false}
-            // trigger={<Button>Basic Modal</Button>}
             >
-                <Header icon>
-                <Icon name='users' circular />
-
+                <Header size='huge' textAlign='center'>
+                ✨ Birthday ✨
                 </Header>
                 <Modal.Content>
-                    <p>Please specify your birthday </p>
-                    <Form id="test2" >
+                    <p>Please specify your date of birth :</p>
+                    <Form error={this.state.futureDOB} >
                         <Form.Group widths='equal'>
                             <Form.Field
                                 control={Select}
@@ -105,22 +104,27 @@ class BirthdayModal extends React.Component {
                                 label='DAY (DD)'
                                 placeholder='DD'
                                 onChange={this.onDayChange.bind(this)}
-                                error= {!this.state.validDay || !this.state.validForm}
+                                error= {this.state.validDay !== null && !this.state.validDay}
                             />
                             <Form.Field
                                 control={Input}
                                 label='YEAR (YYYY)'
                                 placeholder='YYYY'
                                 onChange={this.onYearChange.bind(this)}
-                                error= {!this.state.validYear || !this.state.validForm}
+                                error= {this.state.validYear !== null && !this.state.validYear}
                             />
-
                         </Form.Group>
+
+                        <Message
+                        error
+                        header='Wrong birthday 👺'
+                        content='Please specify a correct date. (This one is in the future)'
+                        />
                     </Form>
 
                 </Modal.Content>
                 <Modal.Actions>
-                <Button color='black' disabled={!this.state.validForm} onClick={this.onConfirm.bind(this)}>
+                <Button color= {this.state.validForm ? 'green' : 'black'} disabled={!this.state.validForm} onClick={this.onConfirm.bind(this)}>
                     Confirm
                 </Button>
                 </Modal.Actions>
